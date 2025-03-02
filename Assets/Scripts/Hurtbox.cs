d8ee22594e6b045c52fa7ab2668c46bf9f778b87
otherotherjosh <itsotherjosh@gmail.com>
2025-03-02 13:40:55 +1300

    feat(hurtbox): add script

---
+++ Assets/Scripts/Hurtbox.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+/// <summary>
+/// Takes damage from a Hitbox component when intersecting.
+/// Requires a trigger collider.
+/// </summary>
+public class Hurtbox : MonoBehaviour
+{
+    [SerializeField] private int health = 100;
+    [SerializeField] private Collider trigger;
+
+    public int Health
+    {
+        get => health; set
+        {
+            health = value;
+        }
+    }
+
+    void Awake()
+    {
+        if (trigger is null)
+        {
+            Debug.LogWarning("No trigger collider found on hitbox. Hitbox disabled.");
+            enabled = false;
+        }
+    }
+}
