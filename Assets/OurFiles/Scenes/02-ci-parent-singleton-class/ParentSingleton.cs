978b8084da7b0a6c314c66776b40c5d555cd6e23
Christian Irvine (1000117593) <IRVICJ5@student.op.ac.nz>
2025-03-25 11:43:47 +1300

    feat: create singleton parent class

---
+++ Assets/OurFiles/Scenes/02-ci-parent-singleton-class/ParentSingleton.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+/// <summary>
+/// A Generic singleton parent class which can be inherited from.
+/// Just keep in mind if you use Awake in the child class, you must call base.Awake() first (or after but probably first in most cases).
+/// </summary>
+/// <typeparam name="T"></typeparam>
+public abstract class ParentSingleton<T> : MonoBehaviour where T : ParentSingleton<T>
+{
+    public static T Instance;
+
+    protected virtual void Awake()
+    {   
+        if (Instance != null && Instance != this as T)
+        {
+            Debug.LogWarning("Two Instances of a singleton class exist. Deleting second of instances on: " + gameObject + ". Other instance can be found at :" + Instance);
+            Destroy(this);
+            return;
+        }
+
+        Instance = this as T;
+    }
+}
