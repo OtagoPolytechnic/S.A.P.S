bb91c40aa8a9e4439a8516049389b72cfad368a1
rohan <rohan.anakin@gmail.com>
2025-03-25 03:11:16 +1300

    feat: finish up debugger

--- Assets/OurFiles/Scripts/Editor/CrowdDebug.cs
+++ Assets/OurFiles/Scripts/Editor/CrowdDebug.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEditor;
+using System.Collections.Generic;
 
 public class CrowdDebug : EditorWindow
 {
@@ -9,27 +10,51 @@ public class CrowdDebug : EditorWindow
         GetWindow<CrowdDebug>("Crowd Debugger");
     }
     public int npcCount = 0;
+    private int spawnPoint = 0;
     private CrowdManager crowdManager;
     public void OnGUI()
     {
+        CrowdSpawner crowdSpawner = FindFirstObjectByType<CrowdSpawner>();
+        GUILayout.Label("Warning: you are able to spawn mulitple copies of crowds on each other which will have unintended side effects", EditorStyles.largeLabel);
+        EditorGUILayout.Space();
         GUILayout.Label("Spawn and manage NPCs", EditorStyles.boldLabel);
         EditorGUILayout.Space();
-
-        npcCount = EditorGUILayout.IntField("NPC Count", npcCount);
-        EditorGUILayout.Space();
-
-        if (GUILayout.Button("Spawn NPC"))
+        if (Application.isPlaying)
         {
-            if (FindFirstObjectByType<CrowdManager>() == null)
+            if (GUILayout.Button("Clear all Active Crowds"))
             {
-                Instantiate(Resources.Load("Crowd"), new Vector3(0,-0.25f,0), Quaternion.identity);
+                GameObject[] crowds = GameObject.FindGameObjectsWithTag("Crowd");
+                foreach (GameObject crowd in crowds)
+                {
+                    Destroy(crowd);
+                }
             }
-            crowdManager = FindFirstObjectByType<CrowdManager>();
-            if (npcCount == 0)
+            EditorGUILayout.Space();
+
+            spawnPoint = EditorGUILayout.IntField("Spawn Point", spawnPoint);
+            GUILayout.Label("NPC Count will always default to have atleast 3 NPCs", EditorStyles.miniLabel);
+            npcCount = EditorGUILayout.IntField("NPC Count", npcCount);
+            EditorGUILayout.Space();
+
+            if (GUILayout.Button("Spawn Individual Crowd"))
             {
-                crowdManager.SpawnGroup();
+                crowdSpawner.SpawnIndividualCrowd(spawnPoint, true);
+                crowdManager = FindFirstObjectByType<CrowdManager>();
+                if (npcCount == 0 || npcCount < 0 || npcCount > 7)
+                {
+                    crowdManager.SpawnGroup();
+                }
+                crowdManager.SpawnGroup(npcCount);
             }
-            crowdManager.SpawnGroup(npcCount);
+
+            if (GUILayout.Button("Spawn All Crowds"))
+            {
+                crowdSpawner.SpawnAllCrowds(new List<int>()); //i pass an empty list to exclude no spawn points
+            }
+        }
+        else
+        {
+            GUILayout.Label("Enter Play Mode to use this tool", EditorStyles.boldLabel); //this is to stop any scary destruction of objects that could cause catastrophy
         }
     }
 }
