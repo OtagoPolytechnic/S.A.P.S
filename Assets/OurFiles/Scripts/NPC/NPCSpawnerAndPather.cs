4ee018a7051b0968c6a5ee92a5561fee5584f89d
rohan <rohan.anakin@gmail.com>
2025-03-26 11:33:43 +1300

    feat: add basic navmesh with npc spawnpoints

---
+++ Assets/OurFiles/Scripts/NPC/NPCSpawnerAndPather.cs
@@ -0,0 +1,10 @@
+using System.Collections.Generic;
+using UnityEngine;
+/// <summary>
+/// Class <c>NPCSpawnerAndPather</c> is used to spawn NPCs and path them to a destination.
+/// </summary>
+public class NPCSpawnerAndPather : MonoBehaviour
+{
+    [SerializeField]
+    private List<GameObject> spawnPoints = new List<GameObject>();
+}
