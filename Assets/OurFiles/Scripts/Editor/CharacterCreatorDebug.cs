335b5a51cdc84e860782742458ca4338091f3315
otherotherjosh <itsotherjosh@gmail.com>
2025-04-07 22:05:58 +1200

    feat(character creator): features stick to surface of body

--- Assets/OurFiles/Scripts/Editor/CharacterCreatorDebug.cs
+++ Assets/OurFiles/Scripts/Editor/CharacterCreatorDebug.cs
@@ -1,5 +1,7 @@
 using UnityEngine;
 using UnityEditor;
+using System.IO;
+using System.Collections.Generic;
 
 // base written by joshii
 
@@ -11,8 +13,10 @@ public class CharacterCreatorDebug : EditorWindow
     // I used this as a base
     // CrowdDebug
 
+    private static CharacterModel model;
+
     [MenuItem("Tools/Character Creator")]
-    public static void ShowEditorWindow()
+    public static void ShowEditorWaindow()
     {
         GetWindow<CharacterCreatorDebug>("Character Creator");
     }
@@ -23,8 +27,41 @@ public class CharacterCreatorDebug : EditorWindow
         if (characterCreator == null)
         {
             Debug.Log("Did not find a creator in scene. Making a new one.");
-            GameObject creatorObject = new GameObject("CharacterCreator");
+            GameObject creatorObject = new("CharacterCreator");
             characterCreator = creatorObject.AddComponent<CharacterCreator>();
         }
+        GUILayout.Space(10);
+        if (GUILayout.Button("Spawn new model"))
+        {
+            if (model != null)
+            {
+                DestroyImmediate(model.gameObject);
+            }
+            model = characterCreator.SpawnCharacterModel();
+        }
+        GUILayout.Space(10);
+        if (model == null)
+        {
+            GUILayout.Label("Spawn a character to edit");
+            return;
+        }
+        GUILayout.Label("Edit character");
+        EditCharacterMenu(characterCreator);
+    }
+
+    void EditCharacterMenu(CharacterCreator creator)
+    {
+        model.Radius = EditorGUILayout.Slider(
+            new GUIContent("Radius"),
+            model.Radius,
+            creator.FeaturePack.minRadius,
+            creator.FeaturePack.maxRadius
+        );
+        model.Height = EditorGUILayout.Slider(
+            new GUIContent("Height"),
+            model.Height,
+            creator.FeaturePack.minHeight,
+            creator.FeaturePack.maxHeight
+        );
     }
 }
