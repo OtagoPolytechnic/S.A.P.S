5358a997c00c771ef4acf88336e588e3701c1d9b
otherotherjosh <itsotherjosh@gmail.com>
2025-04-24 15:08:42 +1200

    feat(NPCVariance): add curve for looking away modifier

--- Assets/LazySusan.cs
+++ Assets/LazySusan.cs
@@ -4,12 +4,13 @@ public class LazySusan : MonoBehaviour
 {
     [SerializeField] private float speedBase = 50f;
     [SerializeField] private float speedLookingAwayModifier = 3;
+    [SerializeField] private AnimationCurve lookingAwayCurve;
 
-    void Update()
+    void FixedUpdate()
     {
         // this wacky calculation computes 0 when looking towards positive Z, 1 when looking towards negative Z
         float lookingAwayAmount = (180 - Mathf.Abs(180 - transform.eulerAngles.y)) / 180f;
-        float lookingAwayModifier = (speedLookingAwayModifier - 1) * lookingAwayAmount;
+        float lookingAwayModifier = (speedLookingAwayModifier - 1) * lookingAwayCurve.Evaluate(lookingAwayAmount);
         float speedModifier = 1 + lookingAwayModifier;
         transform.Rotate(speedBase * speedModifier * Time.deltaTime * Vector3.up);
     }
