c526464640cec4889b6e1e08bcacd26df0508dc1
Christian Irvine (1000117593) <IRVICJ5@student.op.ac.nz>
2025-05-01 17:15:20 +1200

    feat: add distance detection and toggling obstical avoidance

--- Assets/OurFiles/Scripts/Optimizations/PlayerDistanceSphere.cs
+++ Assets/OurFiles/Scripts/Optimizations/PlayerDistanceSphere.cs
@@ -1,26 +1,38 @@
 using UnityEngine;
+using UnityEngine.AI;
 
 /// <summary>
 /// Optimizes certain gameObjects like NPCs based on the distance from the player.
 /// </summary>
 public class PlayerDistanceSphere : MonoBehaviour
 {
-    private const float PHYSICS_SPHERE_SIZE = 50f;
+    private const float PHYSICS_SPHERE_RADIUS = 50f;
     private const string NPC_TAG = "NPC";
+    private SphereCollider trigger;
 
     void Start()
     {
         Collider[] hitColliders;
-        hitColliders = Physics.OverlapSphere(transform.position, PHYSICS_SPHERE_SIZE); // TODO Add a layer mask
+        hitColliders = Physics.OverlapSphere(transform.position, PHYSICS_SPHERE_RADIUS); // TODO Add a layer mask
+        for (int i = hitColliders.Length - 1; i > -1; i--)
+        {
+            SetPerformanceState(hitColliders[i], true);
+        }
+
+        trigger = gameObject.AddComponent<SphereCollider>();
+        trigger.isTrigger = true;
+        trigger.radius = PHYSICS_SPHERE_RADIUS;
     }
 
     private void OnTriggerEnter(Collider other)
     {
+        Debug.Log("In " + other.gameObject);
         SetPerformanceState(other, true);
     }
 
     private void OnTriggerExit(Collider other)
     {
+        Debug.Log("Out " + other.gameObject);
         SetPerformanceState(other, false);
     }
 
@@ -28,7 +40,9 @@ public class PlayerDistanceSphere : MonoBehaviour
     {
         if (other.CompareTag(NPC_TAG))
         {
+            NavMeshAgent agent = other.GetComponent<NavMeshAgent>();
 
+            agent.obstacleAvoidanceType = state ? ObstacleAvoidanceType.LowQualityObstacleAvoidance : ObstacleAvoidanceType.NoObstacleAvoidance;
         }
     }
 }
