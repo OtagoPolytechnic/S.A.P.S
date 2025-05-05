5c676db4d293e50884faabe6dc46d4d7c50e9186
rohan <rohan.anakin@gmail.com>
2025-05-06 10:39:48 +1200

    docs: remove all other files

--- Assets/OurFiles/Scripts/UI/End Game/EndGameMenu.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-public class EndGameMenu : MainMenuManager
-{
-    [SerializeField] private string sceneOnMenu;
-    
-    public void MainMenu() => loader.LoadScene(sceneOnMenu);
-}
