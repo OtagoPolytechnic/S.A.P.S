4dac08c3db6afbb3d4f21416b4a57090e5776e24
lolyay270 <boyeje2@student.op.ac.nz>
2025-05-08 17:42:41 +1200

    feat(contract): toggles for menu

--- Assets/OurFiles/Scripts/Contracts/ContractCardManager.cs
+++ Assets/OurFiles/Scripts/Contracts/ContractCardManager.cs
@@ -5,13 +5,16 @@ public class ContractCardManager : MonoBehaviour
     [SerializeField] private GameObject cardInHand;
     [SerializeField] private ContractInfoCard cardInScene;
     [SerializeField] private GameObject[] leftControllerVisuals;
+    [SerializeField] private PauseManager pauseManager;
     
     private bool isCardVisible = true; 
+    private bool cardVisibleBeforePause;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         cardInScene.onGrab.AddListener(HandleGrab);
+        pauseManager.PauseChange.AddListener(HandlePause);
     }
 
     // Toggles the left controller visuals for the contract card visuals
@@ -26,9 +29,23 @@ public class ContractCardManager : MonoBehaviour
     }
 
     // Destroys the in scene card and enables the card in hand
-    public void HandleGrab() 
+    private void HandleGrab() 
     {
         Destroy(cardInScene.gameObject);
         ToggleVision();
+    }
+
+    private void HandlePause(PauseState state)
+    {
+        if (state == PauseState.Paused && isCardVisible) 
+        {
+            ToggleVision();
+            cardVisibleBeforePause = true;
+        }
+        else if (state == PauseState.Play && cardVisibleBeforePause)
+        {
+            ToggleVision();
+            cardVisibleBeforePause = false; // stop toggle next time menu is closed and card wasnt visible
+        }
     }
 }
