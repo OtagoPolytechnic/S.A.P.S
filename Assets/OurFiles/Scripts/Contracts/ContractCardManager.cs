7eb16f74c902815f6f0139dc516b58d2a42a3783
lolyay270 <boyeje2@student.op.ac.nz>
2025-05-09 10:15:30 +1200

    fix(contract): changing scenes broke everything

--- Assets/OurFiles/Scripts/Contracts/ContractCardManager.cs
+++ Assets/OurFiles/Scripts/Contracts/ContractCardManager.cs
@@ -1,5 +1,7 @@
 using UnityEngine;
 
+// Base written by Jenna 
+
 public class ContractCardManager : MonoBehaviour
 {
     [SerializeField] private GameObject cardInHand;
@@ -7,14 +9,19 @@ public class ContractCardManager : MonoBehaviour
     [SerializeField] private GameObject[] leftControllerVisuals;
     [SerializeField] private PauseManager pauseManager;
     
-    private bool isCardVisible = true; 
+    private bool isCardVisible = false; 
     private bool cardVisibleBeforePause;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        cardInScene.onGrab.AddListener(HandleGrab);
-        pauseManager.PauseChange.AddListener(HandlePause);
+        // objects not on the player need to be found on new scene loads
+        if (cardInScene == null) cardInScene = FindFirstObjectByType<ContractInfoCard>();
+        if (pauseManager == null) pauseManager = FindFirstObjectByType<PauseManager>();
+        
+        // stop null exceptions in scenes that shouldnt have cardInScene or pause (eg main menu)
+        if (cardInScene != null) cardInScene.onGrab.AddListener(HandleGrab);
+        if (pauseManager != null) pauseManager.PauseChange.AddListener(HandlePause);
     }
 
     // Toggles the left controller visuals for the contract card visuals
@@ -35,6 +42,7 @@ public class ContractCardManager : MonoBehaviour
         ToggleVision();
     }
 
+    // Toggles the display of the contract card when pause menu is opened/closed
     private void HandlePause(PauseState state)
     {
         if (state == PauseState.Paused && isCardVisible) 
