594255249f932fe84d453d69728ccd0f41982ab7
Christian Irvine (1000117593) <IRVICJ5@student.op.ac.nz>
2025-05-13 10:33:23 +1200

    feat: add voice pack scriptable object

---
+++ Assets/OurFiles/Scripts/NPC/CharacterCreator/CharacterVoicePack.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+// Base written by Christian.
+
+[CreateAssetMenu(fileName = "CharacterVoicePack", menuName = "Scriptable Objects/CharacterVoicePack")]
+public class CharacterVoicePack : ScriptableObject
+{
+    
+}
