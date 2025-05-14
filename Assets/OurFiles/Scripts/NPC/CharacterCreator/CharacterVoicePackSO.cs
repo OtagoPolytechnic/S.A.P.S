0b710daf929d44d0464857c2fcc8bc541edd894e
Christian Irvine (1000117593) <IRVICJ5@student.op.ac.nz>
2025-05-14 16:11:52 +1200

    feat: add all voice line examples

--- Assets/OurFiles/Scripts/NPC/CharacterCreator/CharacterVoicePackSO.cs
+++ Assets/OurFiles/Scripts/NPC/CharacterCreator/CharacterVoicePackSO.cs
@@ -17,9 +17,23 @@ public class CharacterVoicePackSO : ScriptableObject
     [Tooltip("Lines for when a guard is chasing the player e.g \"Hey, get back here!\"")]
     public AudioClip[] guardChase;
 
+    [Header("Leader/Follower")]
+    [Tooltip("When a Leader or Follower is pathing e.g \"Do you guys want to head to the restaurant?\"")]
+    public AudioClip[] leaderPathing;
+
     [Header("Other NPCs")]
+    [Tooltip("When an NPC is pathing to an edge of scene e.g \"So excited to go to the supermarket!\"")]
+    public AudioClip[] baseLeaveScene;
+    [Tooltip("When an NPC is pathing to a crowd (Not followers/leaders) e.g \"So excited to see my buddies!\"")]
+    public AudioClip[] baseToCrowd;
+    [Tooltip("When an NPC is sitting in a crowd e.g \"Are you going to the bash tonight? It's going to be kicking!\"")]
+    public AudioClip[] baseInCrowd;
+    [Tooltip("When an NPC is getting suspicious of the player (Not guard) e.g \"Huh, what an odd fellow.\"")]
+    public AudioClip[] baseSuspicion;
+    [Tooltip("When an NPC is panicking (Not guard) e.g \"Oh golly! Get out of here!\"")]
+    public AudioClip[] basePanic;
 
     [Header("All NPCs")]
-    [Tooltip("Lines for when the NPC dies e.g \"Bleaughh\"")]
-    public AudioClip[] npcDie;
+    [Tooltip("Lines for when an NPC dies e.g \"Bleaughh\"")]
+    public AudioClip[] allDie;
 }
