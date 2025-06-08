af8c26148d900dae4a9e6af874266d8e9be33bed
rohan <rohan.anakin@gmail.com>
2025-06-08 18:58:48 +1200

    fix: wrong colliders being chosen for player in vision

--- Assets/OurFiles/Scripts/Tutorial/TutorialNPCRespawner.cs
+++ Assets/OurFiles/Scripts/Tutorial/TutorialNPCRespawner.cs
@@ -24,7 +24,7 @@ public class TutorialNPCRespawner : MonoBehaviour
 
     void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.CompareTag("Player"))
+        if (other.gameObject.CompareTag("Hand"))
         {
             RespawnNPCs();
         }
