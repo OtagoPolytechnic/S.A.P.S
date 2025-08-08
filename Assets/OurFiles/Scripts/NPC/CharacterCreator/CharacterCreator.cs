8a7d63763a4e62e680cd14be3a4bf3f507c79e1c
Amber <amberj3lly@gmail.com>
2025-08-08 21:36:32 +1200

    fix: I have been using godot for to long and i used var i forgor that we are in unity forgive me mighty ones have mercy

--- Assets/OurFiles/Scripts/NPC/CharacterCreator/CharacterCreator.cs
+++ Assets/OurFiles/Scripts/NPC/CharacterCreator/CharacterCreator.cs
@@ -91,7 +91,7 @@ public class CharacterCreator : MonoBehaviour
         }
 
         // init expression controller so it knows this NPC's default face
-        var expr = parent.GetComponent<NPCExpressionController>();
+        NPCExpressionController expr = parent.GetComponent<NPCExpressionController>();
         if (expr != null)
         {
 	        expr.FeaturePack = featurePack;
@@ -136,7 +136,7 @@ public class CharacterCreator : MonoBehaviour
         }
 
         // init expression controller for the target
-        var expr = parent.GetComponent<NPCExpressionController>();
+        NPCExpressionController expr = parent.GetComponent<NPCExpressionController>();
         if (expr != null)
         {
             expr.FeaturePack = featurePack;
