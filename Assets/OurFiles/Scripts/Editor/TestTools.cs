a482485ef212d90be32f1461511242c7e053e688
otherotherjosh <132322996+otherotherjosh@users.noreply.github.com>
2025-08-27 15:20:54 +1200

    feat: enable beacon in tool

--- Assets/OurFiles/Scripts/Editor/TestTools.cs
+++ Assets/OurFiles/Scripts/Editor/TestTools.cs
@@ -1,14 +1,24 @@
+using System.Runtime.Remoting.Messaging;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit.Inputs.Simulation;
+using System.Collections.Generic;
 
 /// <summary>
 /// A suite of popular tools to aid gameplay testing
 /// </summary>
 public class GameplayTestTools : EditorWindow
 {
+    private Vector2 scrollPosition;
+
+    private bool showXRSection = true;
     private XRDeviceSimulator simulator;
     private bool useXRSimulator;
+
+    private bool showTargetHelper = true;
+    private GameObject targetNPC;
+    private GameObject beacon;
+    private bool enableTargetBeacon;
 
     [MenuItem("Tools/Gameplay Test Tools")]
     static void ShowEditorWindow()
@@ -18,12 +28,25 @@ public class GameplayTestTools : EditorWindow
 
     void OnGUI()
     {
-        useXRSimulator = EditorGUILayout.Toggle("Use XR Device Simulator", useXRSimulator);
+        scrollPosition = EditorGUILayout.BeginScrollView(scrollPosition);
+        EditXRSimulator();
+        TargetNPCHelper();
+        EditorGUILayout.EndScrollView();
+    }
 
-        if (!Application.isPlaying)
+    void EditXRSimulator()
+    {
+        EditorGUILayout.Space();
+        showXRSection = EditorGUILayout.BeginFoldoutHeaderGroup(showXRSection, "XR");
+
+        if (showXRSection)
         {
-            return;
+            useXRSimulator = EditorGUILayout.Toggle("Use XR Device Simulator", useXRSimulator);
         }
+
+        EditorGUILayout.EndFoldoutHeaderGroup();
+        
+        if (!Application.isPlaying) return; 
 
         if (simulator == null)
         {
@@ -33,9 +56,38 @@ public class GameplayTestTools : EditorWindow
             DontDestroyOnLoad(simulator);
         }
 
-        if (simulator.gameObject.activeSelf != useXRSimulator)
+        simulator.gameObject.SetActive(useXRSimulator);
+    }
+
+    void TargetNPCHelper()
+    {
+        EditorGUILayout.Space();
+        showTargetHelper = EditorGUILayout.BeginFoldoutHeaderGroup(showTargetHelper, "Target NPC");
+
+        if (showTargetHelper)
         {
-            simulator.gameObject.SetActive(useXRSimulator);
+            enableTargetBeacon = EditorGUILayout.Toggle("Display beacon", enableTargetBeacon);
+        }
+
+        if (!Application.isPlaying) return;
+
+        if (targetNPC == null) targetNPC = GameObject.Find("TargetNPC");
+
+        if (enableTargetBeacon)
+        {
+            if (beacon == null) 
+            {
+                beacon = Instantiate(AssetDatabase.LoadAssetAtPath<GameObject>(
+                    "Assets/OurFiles/prefabs/Beacon.prefab"
+                ));
+                beacon.transform.SetParent(targetNPC.transform);
+                beacon.transform.localPosition = Vector3.zero;
+            }
+            beacon.SetActive(true);
+        }
+        else
+        {
+            beacon.SetActive(false);
         }
     }
 }
