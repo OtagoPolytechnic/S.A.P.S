bd2788d6b9a1818863291523d23db43dd78d3b9e
otherotherjosh <132322996+otherotherjosh@users.noreply.github.com>
2025-08-31 22:20:51 +1200

    feat: display text feedback on target kill

--- Assets/OurFiles/Scripts/WorldSpaceEventFeedback.cs
+++ Assets/OurFiles/Scripts/WorldSpaceEventFeedback.cs
@@ -1,4 +1,6 @@
 using System.Collections;
+using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class WorldSpaceEventFeedback : MonoBehaviour
@@ -12,18 +14,85 @@ public class WorldSpaceEventFeedback : MonoBehaviour
     [SerializeField] float sphereRadiusAnimationDuration;
     [SerializeField, Range(0.01f, 1)] float closedTimeScale;
     [SerializeField] float timeScaleAnimationDuration;
+    [SerializeField] TextMeshPro textMeshPro;
 
-    void Start()
+    private Vector3 textLocalPosition;
+
+    IEnumerator Start()
     {
         // set sphere material to instance so we don't modify the one in the asset database
         playerSphere.material = new Material(playerSphere.material);
         playerSphere.material.SetFloat("_Alpha", 0);
+        playerSphere.transform.parent = Camera.main.transform;
         playerSphere.transform.localScale = Vector3.one * sphereOpenRadius;
+        playerSphere.transform.localPosition = Vector3.zero;
         Time.timeScale = 1;
+        textLocalPosition = textMeshPro.transform.localPosition;
 
-        // temporary
-        StartCoroutine(TestCoroutine());
+        while (NPCSpawner.Instance.Target == null)
+        {
+            yield return null;
+        }
+        NPCSpawner.Instance.Target.GetComponent<Hurtbox>().onDie.AddListener(targetObj =>
+        {
+            DisplayFeedback(new[] { "Go back to the elevator", "Don't get caught" }, new[] { GameObject.Find("SAPS Building") }, true);
+        });
     }
+
+    public void DisplayFeedback(string[] feedback, GameObject[] overlayObjects, bool overlayChildObjects = true)
+    {
+        Dictionary<GameObject, int> objectLayers = new(); // remember what layer the objects are originally on
+        foreach (GameObject obj in overlayObjects)
+        {
+            GetObjectLayers(obj, objectLayers, overlayChildObjects);
+        }
+        StartCoroutine(DisplayFeedbackCoroutine(feedback, objectLayers));
+    }
+
+    void GetObjectLayers(GameObject obj, Dictionary<GameObject, int> objectLayers, bool includeChildren = true)
+    {
+        objectLayers.Add(obj, obj.layer);
+        if (includeChildren)
+        {
+            foreach (Transform childTransform in obj.transform)
+            {
+                GetObjectLayers(childTransform.gameObject, objectLayers, true);
+            }
+        }
+    }
+
+    IEnumerator DisplayFeedbackCoroutine(string[] feedback, Dictionary<GameObject, int> objectLayers)
+    {
+        StartCoroutine(CloseSphereCoroutine());
+        yield return new WaitForSecondsRealtime(2);
+        foreach (GameObject obj in objectLayers.Keys)
+        {
+            obj.layer = LayerMask.NameToLayer("Overlay");
+        }
+        foreach (string feedbackString in feedback)
+        {
+            DisplayText(feedbackString);
+            yield return new WaitForSecondsRealtime(2.4f);
+            HideText();
+            yield return new WaitForSecondsRealtime(0.4f);
+        }
+        foreach (GameObject obj in objectLayers.Keys)
+        {
+            obj.layer = objectLayers[obj];
+        }
+        yield return StartCoroutine(OpenSphereCoroutine());
+    }
+
+    void DisplayText(string text)
+    {
+        textMeshPro.transform.parent = Camera.main.transform;
+        textMeshPro.transform.localPosition = textLocalPosition;
+        textMeshPro.text = text;
+        textMeshPro.gameObject.SetActive(true);
+        textMeshPro.transform.parent = transform;
+    }
+
+    void HideText() => textMeshPro.gameObject.SetActive(false);
 
     IEnumerator OpenSphereCoroutine()
     {
@@ -88,20 +157,7 @@ public class WorldSpaceEventFeedback : MonoBehaviour
             animValue += Time.unscaledDeltaTime / timeScaleAnimationDuration;
             animValue = Mathf.Clamp(animValue, 0, 1);
             Time.timeScale = Mathf.Lerp(startScale, targetTimeScale, animValue);
-            print("time scale: " + Time.timeScale);
             yield return null;
-        }
-    }
-
-    IEnumerator TestCoroutine()
-    {
-        yield return new WaitForSecondsRealtime(1);
-        while (true)
-        {
-            yield return StartCoroutine(CloseSphereCoroutine());
-            yield return new WaitForSecondsRealtime(1);
-            yield return StartCoroutine(OpenSphereCoroutine());
-            yield return new WaitForSecondsRealtime(1);
         }
     }
 }
