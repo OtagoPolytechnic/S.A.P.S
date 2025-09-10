3634a801575f4209bb6a6ebbe593e9fa30e9f0db
lolyay270 <boyeje2@student.op.ac.nz>
2025-09-10 18:54:15 +1200

    feat(pause): settings button changes content

--- Assets/OurFiles/Scripts/UI/Pause/PauseMenuManager.cs
+++ Assets/OurFiles/Scripts/UI/Pause/PauseMenuManager.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using TMPro;
 
 /// <summary>
 /// Manages the UI of the pause menu when its enabled.
@@ -6,6 +7,11 @@ using UnityEngine;
 public class PauseMenuManager : MonoBehaviour
 {
     [SerializeField] private Transform cam;
+
+    [SerializeField] private GameObject normalMenuContent;
+    [SerializeField] private GameObject settingsMenuContent;
+    [SerializeField] private TMP_Text menuTitle;
+    
 
     private void Start()
     {
@@ -36,5 +42,12 @@ public class PauseMenuManager : MonoBehaviour
     public void Resume()
     {
         PauseManager.Instance.State = PauseState.Play;
+    }
+
+    public void Settings()
+    {
+        normalMenuContent.SetActive(!normalMenuContent.activeSelf);
+        settingsMenuContent.SetActive(!normalMenuContent.activeSelf);
+        menuTitle.text = "SETTINGS";
     }
 }
