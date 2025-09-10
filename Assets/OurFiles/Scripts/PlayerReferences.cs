3fe12af6b11b48d21113abd80de91c261b78b1d4
lolyay270 <boyeje2@student.op.ac.nz>
2025-09-10 19:51:42 +1200

    feat(player): references script, in prefab, assigned

---
+++ Assets/OurFiles/Scripts/PlayerReferences.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit.Locomotion.Turning;
+
+public class PlayerReferences : MonoBehaviour
+{
+    [SerializeField] SnapTurnProvider snapTurn;
+    [SerializeField] ContinuousTurnProvider constTurn;
+}
