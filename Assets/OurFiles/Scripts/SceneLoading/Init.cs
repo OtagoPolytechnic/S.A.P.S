e63616c56febd7c229ed3defd818ea530e8de331
otherotherjosh <132322996+otherotherjosh@users.noreply.github.com>
2025-09-10 12:44:47 +1200

    fix: init loads active scene in edit mode

--- Assets/OurFiles/Scripts/SceneLoading/Init.cs
+++ Assets/OurFiles/Scripts/SceneLoading/Init.cs
@@ -1,3 +1,4 @@
+using UnityEditorInternal;
 using UnityEngine;
 
 /// <summary>
@@ -9,6 +10,12 @@ public class Init : MonoBehaviour
 
     void Start()
     {
+#if UNITY_EDITOR
+        if (EditModeSceneLoader.LoadInitSceneOnPlay && EditModeSceneLoader.ActiveSceneInEditMode != "Init")
+        {
+            sceneOnStart = EditModeSceneLoader.ActiveSceneInEditMode;
+        }
+#endif
         StartCoroutine(SceneLoader.Instance.LoadSceneAsync(sceneOnStart));
     }
 }
