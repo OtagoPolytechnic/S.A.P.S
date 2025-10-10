f60c738e44c68c8c0dad636488f19d7d4fa05fa4
lolyay270 <boyeje2@student.op.ac.nz>
2025-10-10 16:14:26 +1300

    doc(guard): XML doc

--- Assets/OurFiles/Scripts/NPC/Behaviours/GuardFollower.cs
+++ Assets/OurFiles/Scripts/NPC/Behaviours/GuardFollower.cs
@@ -45,7 +45,7 @@ public class GuardFollower : Follower
         //stop the guard running away
         NPCEventManager.Instance.onPanic?.Invoke(gameObject);
     }
-    
+
     void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Player") && guardLeader.IsChasing)
@@ -53,7 +53,11 @@ public class GuardFollower : Follower
             NPCEventManager.Instance.onPlayerArrested?.Invoke();
         }
     }
-    
+
+    /// <summary>
+    /// Change which game object this follower paths towards
+    /// </summary>
+    /// <param name="leader"></param>
     public void SetLeader(GameObject leader)
     {
         FollowLeader(leader, homePoint);
